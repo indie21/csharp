60e3c1907f6fcce5da1f04c6ab1cde7e25acdd8d
zhuoyikang <zhuoyikang@gmail.com>
2016-05-14 15:57:17 +0800

    socket

--- hello.cs
+++ hello.cs
@@ -4,6 +4,9 @@ public class HelloWorld
 {
     static public void Main()
     {
+        var name = "Good";
+        Type nameType = name.GetType();
+        Console.WriteLine("name type is "+nameType.ToString());
         Console.WriteLine("Hello，World");
     }
 }
---
+++ socket.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace Net
+{
+    public class Test
+    {
+        static public void Main()
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork,
+                                             SocketType.Stream,
+                                             ProtocolType.Tcp);
+            try
+            {
+                clientSocket.Connect(new IPEndPoint(ip, 8881)); //配置服务器IP与端口
+                Console.WriteLine("连接服务器成功");
+            }
+            catch
+            {
+                Console.WriteLine("连接服务器失败，请按回车键退出！");
+            }
+            Console.ReadKey();
+        }
+    }
+}
