71713ca7c0e3ec0d013f25d8ff08488b4a3cbf76
zhuoyikang <zhuoyikang@gmail.com>
2016-05-14 17:02:16 +0800

    add bak

--- SlashNet/SlashNet/Program.cs
+++ SlashNet/SlashNet/Program.cs
@@ -2,11 +2,13 @@
 
 namespace Slash
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello World!");
-		}
-	}
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Net net = new Net();
+            net.Connect("127.0.0.1", 8881);
+            Console.ReadKey();
+        }
+    }
 }
--- SlashNet/SlashNet/SlashNet.cs
+++ SlashNet/SlashNet/SlashNet.cs
@@ -6,41 +6,41 @@ using System.Threading;
 
 namespace Slash
 {
-	public class Net
-	{
-		private Socket clientSocket;
-		//private ByteBuf buf;
+    public class Net
+    {
+        private Socket clientSocket;
+        private ByteBuf buf;
 
+        public void Connect(string ip, int port)
+        {
+            this.clientSocket = new Socket(AddressFamily.InterNetwork,
+                                           SocketType.Stream,
+                                           ProtocolType.Tcp);
 
-		public void Connect(string ip, int port)
-		{
-			this.clientSocket = new Socket(AddressFamily.InterNetwork,
-				SocketType.Stream,
-				ProtocolType.Tcp);
+            this.clientSocket.NoDelay = true;
+            LingerOption linger = new LingerOption(false, 0);
+            this.clientSocket.LingerState = linger;
+            this.clientSocket.BeginConnect(ip, port, connected_cb, this);
+        }
 
-			this.clientSocket.NoDelay = true;
-			LingerOption linger = new LingerOption(false, 0);
-			this.clientSocket.LingerState = linger;
-			this.clientSocket.BeginConnect(ip, port, connected_cb, this);
-		}
+        private void connected_cb(IAsyncResult ar)
+        {
+            if (!this.clientSocket.Connected)
+            {
+                return;
+            }
 
-		private void connected_cb(IAsyncResult ar)
-		{
-			if (!this.clientSocket.Connected)
-			{
-				return;
-			}
+            this.clientSocket.EndConnect(ar);
+            Thread thread = new Thread(new ThreadStart(receive));
+            thread.IsBackground = true;
+            thread.Start();
+        }
 
-			this.clientSocket.EndConnect(ar);
-			Thread thread = new Thread(new ThreadStart(receive));
-			thread.IsBackground = true;
-			thread.Start();
-		}
+        // 异步收包线程.
+        private void receive()
+        {
+            Console.WriteLine("receiver2");
 
-		// 异步收包线程.
-		private void receive()
-		{
-			Console.WriteLine("receive");
-		}
-	}
+        }
+    }
 }
