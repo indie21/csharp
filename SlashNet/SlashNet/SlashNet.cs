c2f9e18747cb1e97f86737a75350361a25d860fd
zhuoyikang <zhuoyikang@gmail.com>
2016-05-14 20:09:07 +0800

    可以接受服务器的消息

--- SlashNet/SlashNet/SlashNet.cs
+++ SlashNet/SlashNet/SlashNet.cs
@@ -3,13 +3,22 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Diagnostics;
 
 namespace Slash
 {
     public class Net
     {
+
+        const int BUFF_SIZE =  65535;
+        const int HEAD_SIZE =  4;
         private Socket clientSocket;
         private ByteBuf buf;
+
+        public Net()
+        {
+            buf = new ByteBuf(BUFF_SIZE);
+        }
 
         public void Connect(string ip, int port)
         {
@@ -31,16 +40,61 @@ namespace Slash
             }
 
             this.clientSocket.EndConnect(ar);
-            Thread thread = new Thread(new ThreadStart(receive));
+            Thread thread = new Thread(new ThreadStart(waitSocket));
             thread.IsBackground = true;
             thread.Start();
         }
 
-        // 异步收包线程.
-        private void receive()
+        public void Close()
         {
-            Console.WriteLine("receiver2");
-
+            if (clientSocket != null && clientSocket.Connected)
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
         }
+
+        // 异步收包线程.
+        private void waitSocket()
+        {
+            Console.WriteLine("receiver {0}", 1);
+            int len;
+
+            while(true)
+            {
+                if (!this.clientSocket.Poll(-1, SelectMode.SelectRead))
+                {
+                    Console.WriteLine("poll error");
+                    this.Close();
+                }
+                
+                len = clientSocket.Receive(buf.GetRaw(), 0, HEAD_SIZE,
+                                           SocketFlags.None);
+
+                Console.WriteLine("receive length 1 {0}", len);
+
+                if(len<0)
+                {
+                    Console.WriteLine("length error");
+                    this.Close();
+                    return;
+                }
+
+                Debug.Assert(len == HEAD_SIZE);
+                
+                int payload_length = buf.GetInt(0);
+                
+                Debug.Assert(payload_length < BUFF_SIZE);
+ 
+                len = clientSocket.Receive(buf.GetRaw(),0, payload_length,
+                                           SocketFlags.None);
+
+                Console.WriteLine("receive length 2 {0}", len);
+
+                Debug.Assert(payload_length == len);
+                string data = BitConverter.ToString(buf.GetRaw(), 0, payload_length);
+                Console.WriteLine("receiver: {0}", data);
+            }
+        }            
     }
 }
